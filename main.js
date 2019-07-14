a8e2196a4128c90508d30cbde9861aa74ce4d3bd
Briana Bazail <brianabazail@ufl.edu>
2019-07-14 19:57:24 -0400

    Add color change to buttons through js

---
+++ main.js
@@ -0,0 +1,63 @@
+//Pop up after clicking 'VIEW' on home page.//
+var ext_link = document.getElementById('external_link');
+ext_link.addEventListener('click',
+  function(event) {
+      var is_sure = confirm('This link will take you to an external page.');
+      if (is_sure === false) {
+        event.preventDefault();
+        alert ('Happy to have you stay.');
+    }
+  }, false
+);
+
+//More text appears after clicking 'LEARN MORE'//
+
+
+//Button colors change when hovered over in stories page.
+document.getElementById('button1').addEventListener('mouseenter',
+  function() {
+    document.getElementById('button1').style.backgroundColor = '#D8D0C1';
+  }, false
+);
+
+document.getElementById('button1').addEventListener('mouseleave',
+  function() {
+    document.getElementById('button1').style.backgroundColor = '#416788';
+  }, false
+);
+
+document.getElementById('button2').addEventListener('mouseenter',
+  function() {
+    document.getElementById('button2').style.backgroundColor = '#D8D0C1';
+  }, false
+);
+
+document.getElementById('button2').addEventListener('mouseleave',
+  function() {
+    document.getElementById('button2').style.backgroundColor = '#416788';
+  }, false
+);
+
+document.getElementById('button3').addEventListener('mouseenter',
+  function() {
+    document.getElementById('button3').style.backgroundColor = '#D8D0C1';
+  }, false
+);
+
+document.getElementById('button3').addEventListener('mouseleave',
+  function() {
+    document.getElementById('button3').style.backgroundColor = '#416788';
+  }, false
+);
+
+document.getElementById('button4').addEventListener('mouseenter',
+  function() {
+    document.getElementById('button4').style.backgroundColor = '#D8D0C1';
+  }, false
+);
+
+document.getElementById('button4').addEventListener('mouseleave',
+  function() {
+    document.getElementById('button4').style.backgroundColor = '#416788';
+  }, false
+);
